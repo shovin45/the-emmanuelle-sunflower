b6f6cb49ac3d2866c47bc9f85e20122d22202306
shovin45 <shovin36@gmail.com>
2020-03-03 01:41:15 +0900

    swiper動くかな？コンポネ追加した

--- pages/index.vue
+++ pages/index.vue
@@ -53,9 +53,10 @@ div
 <script>
 import TheHeader from '~/components/TheHeader.vue'
 
+
 export default {
   components: {
-    TheHeader
+    TheHeader,
   },
   data() {
     return {
--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -2,3 +2,11 @@ import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.use(VueAwesomeSwiper)
+
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
